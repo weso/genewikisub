9c188b3628c4c14c164f77dea1b4ee4be76e28cc
mistermboy <pabloyo97@hotmail.com>
2021-09-15 15:15:22 +0200

    entrypoint

--- shex/subclasses/sparql/scripts/utils/FileUtils.js
+++ shex/subclasses/sparql/scripts/utils/FileUtils.js
@@ -1,16 +1,6 @@
 import fs from 'fs';
 
 export class FileUtils{
-    static getFileContent(path){
-        let data;
-        try {
-            data = fs.readFileSync(path, 'utf8')
-          } catch (err) {
-            console.error(err)
-          }
-          return data;
-    }
-
     static writeFile(path,content){
         try {
             fs.writeFileSync(path, content);
